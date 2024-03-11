287e2a48f6638867388abb1f18caf4d4e9cb11ed
jkris347 <jkris347@gmail.com>
2024-03-11 18:22:17 +0800

    first commit

---
+++ src/navigation/defaultVertical/index.ts
@@ -0,0 +1,56 @@
+// ** Type import
+import { VerticalNavItemsType } from 'src/@core/layouts/types'
+
+const navigation = (): VerticalNavItemsType => {
+  return [
+    {
+      title: 'Dashboards',
+      icon: 'mdi:home-outline',
+      children: [
+        {
+          title: 'Analytics',
+          path: '/dashboards/analytics'
+        },
+      ]
+    },
+
+    {
+      title: 'User',
+      icon: 'mdi:account-outline',
+      children: [
+        {
+          title: 'List',
+          path: '/apps/user/list'
+        },
+        // {
+        //   title: 'View',
+        //   children: [
+        //     {
+        //       title: 'Overview',
+        //       path: '/apps/user/view/overview'
+        //     },
+        //     {
+        //       title: 'Security',
+        //       path: '/apps/user/view/security'
+        //     },
+        //     {
+        //       title: 'Billing & Plans',
+        //       path: '/apps/user/view/billing-plan'
+        //     },
+        //     {
+        //       title: 'Notifications',
+        //       path: '/apps/user/view/notification'
+        //     },
+        //     {
+        //       title: 'Connection',
+        //       path: '/apps/user/view/connection'
+        //     }
+        //   ]
+        // }
+      ]
+    },
+
+  ]
+}
+
+export default navigation
